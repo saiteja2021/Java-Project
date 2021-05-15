379f1c42d98ccc7285a4c6c2d9af5cf2cb2b7955
saiteja <saiteja2021@gmail.com>
2021-05-15 22:55:31 +0530

    cmt

--- src/com/sai/javainterviewprograms/AccoliteDigital.java
+++ src/com/sai/javainterviewprograms/AccoliteDigital.java
@@ -5,7 +5,7 @@ public class AccoliteDigital {
 	public static void main(String[] args) {
 		String s = "hai this is 123hyderabad456";
 		s = s.replaceAll(" ", "");
-		s = s.replaceAll("[a-z]", "");
+		s = s.replaceAll("[0-9]", "");
 		s=s.replace('1', '2');
 		System.out.println(s);
 	}
